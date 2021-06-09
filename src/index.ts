4c09febecb7a3dd56320947c2a1dc154f35738cb
Brian Thai <brian.thai@rdbrck.com>
2021-06-09 14:54:14 -0700

    linting

--- src/index.ts
+++ src/index.ts
@@ -12,25 +12,25 @@ export default function register(vjs: typeof videojs = videojs) {
     class Plugin extends vjsPlugin implements PrebidOutStreamPlugin.Instance {
         player: VideoJsPlayer;
         options?: PrebidOutStreamPlugin.Options;
-    
+
         constructor(player: VideoJsPlayer, options?: PrebidOutStreamPlugin.Options) {
             super(player);
-    
+
             this.player = player;
             this.options = options;
-    
+
             this.parse();
         }
-    
+
         parse = async () => {
             const vp = new VASTParser();
-    
+
             try {
                 if (this.options?.adTagUrl) {
                     const response = await vp.getAndParseVAST(this.options.adTagUrl, this.options);
                     this.display(response);
                 }
-    
+
                 if (this.options?.adXml) {
                     // Needs try catch block to catch parser errors
                     const xmlParser = new DOMParser();
@@ -43,18 +43,18 @@ export default function register(vjs: typeof videojs = videojs) {
                 this.trigger('outstream.error', e);
             }
         };
-    
+
         isLinearCreative(creative: any): creative is VastCreativeLinear {
             return creative?.mediaFiles !== undefined;
         }
-    
+
         display(response: VastResponse) {
             const creative = response.ads?.[0].creatives?.[0];
             if (!this.isLinearCreative(creative)) {
                 // non linear creative
                 return;
             }
-    
+
             if (creative.apiFramework === 'VPAID') {
                 if (this.options?.useVPAID) {
                     // Resolve VPAID
@@ -66,10 +66,10 @@ export default function register(vjs: typeof videojs = videojs) {
                 const source = this.player.selectSource(creative.mediaFiles);
                 this.player.preload(true);
                 this.player.src(source);
-    
+
                 // Autoplay
             }
-    
+
             // Play all ads up until max duration
         }
     }
