3dfaf0e2f92901947fe0eea47309ccfe53c61505
Brian Thai <brian.thai@rdbrck.com>
2021-06-09 14:52:12 -0700

    Next class definition and create dependency on videojs plugin.

--- src/index.ts
+++ src/index.ts
@@ -6,72 +6,73 @@ import { VPAIDParser } from './vpaid';
 // Add logger for debug option
 // When destroyed, clear all listeners on window
 // When destroyed, clear vpaid container
+export default function register(vjs: typeof videojs = videojs) {
+    const vjsPlugin = vjs.getPlugin('plugin');
 
-export class Plugin extends videojs.Plugin implements PrebidOutStreamPlugin.Instance {
-    player: VideoJsPlayer;
-    options?: PrebidOutStreamPlugin.Options;
-
-    constructor(player: VideoJsPlayer, options?: PrebidOutStreamPlugin.Options) {
-        super(player);
-
-        this.player = player;
-        this.options = options;
-
-        this.parse();
+    class Plugin extends vjsPlugin implements PrebidOutStreamPlugin.Instance {
+        player: VideoJsPlayer;
+        options?: PrebidOutStreamPlugin.Options;
+    
+        constructor(player: VideoJsPlayer, options?: PrebidOutStreamPlugin.Options) {
+            super(player);
+    
+            this.player = player;
+            this.options = options;
+    
+            this.parse();
+        }
+    
+        parse = async () => {
+            const vp = new VASTParser();
+    
+            try {
+                if (this.options?.adTagUrl) {
+                    const response = await vp.getAndParseVAST(this.options.adTagUrl, this.options);
+                    this.display(response);
+                }
+    
+                if (this.options?.adXml) {
+                    // Needs try catch block to catch parser errors
+                    const xmlParser = new DOMParser();
+                    const doc = xmlParser.parseFromString(this.options.adXml, 'text/xml');
+                    const response = await vp.parseVAST(doc, this.options);
+                    this.display(response);
+                }
+            } catch (e) {
+                // Ad error
+                this.trigger('outstream.error', e);
+            }
+        };
+    
+        isLinearCreative(creative: any): creative is VastCreativeLinear {
+            return creative?.mediaFiles !== undefined;
+        }
+    
+        display(response: VastResponse) {
+            const creative = response.ads?.[0].creatives?.[0];
+            if (!this.isLinearCreative(creative)) {
+                // non linear creative
+                return;
+            }
+    
+            if (creative.apiFramework === 'VPAID') {
+                if (this.options?.useVPAID) {
+                    // Resolve VPAID
+                    const parser = new VPAIDParser(creative);
+                    parser.inject(this.player.el());
+                }
+            } else {
+                // Resolve vast
+                const source = this.player.selectSource(creative.mediaFiles);
+                this.player.preload(true);
+                this.player.src(source);
+    
+                // Autoplay
+            }
+    
+            // Play all ads up until max duration
+        }
     }
 
-    parse = async () => {
-        const vp = new VASTParser();
-
-        try {
-            if (this.options?.adTagUrl) {
-                const response = await vp.getAndParseVAST(this.options.adTagUrl, this.options);
-                this.display(response);
-            }
-
-            if (this.options?.adXml) {
-                // Needs try catch block to catch parser errors
-                const xmlParser = new DOMParser();
-                const doc = xmlParser.parseFromString(this.options.adXml, 'text/xml');
-                const response = await vp.parseVAST(doc, this.options);
-                this.display(response);
-            }
-        } catch (e) {
-            // Ad error
-            this.trigger('outstream.error', e);
-        }
-    };
-
-    isLinearCreative(creative: any): creative is VastCreativeLinear {
-        return creative?.mediaFiles !== undefined;
-    }
-
-    display(response: VastResponse) {
-        const creative = response.ads?.[0].creatives?.[0];
-        if (!this.isLinearCreative(creative)) {
-            // non linear creative
-            return;
-        }
-
-        if (creative.apiFramework === 'VPAID') {
-            if (this.options?.useVPAID) {
-                // Resolve VPAID
-                const parser = new VPAIDParser(creative);
-                parser.inject(this.player.el());
-            }
-        } else {
-            // Resolve vast
-            const source = this.player.selectSource(creative.mediaFiles);
-            this.player.preload(true);
-            this.player.src(source);
-
-            // Autoplay
-        }
-
-        // Play all ads up until max duration
-    }
-}
-
-export default function register(vjs: typeof videojs) {
     vjs.registerPlugin('outstream', Plugin);
 }
