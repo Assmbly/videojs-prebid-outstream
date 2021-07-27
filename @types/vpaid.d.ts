86a9e7b5d3e0d454162faa7a61d2de76c162a22d
Brian <brian.thai@rdbrck.com>
2021-07-27 11:15:52 -0700

    Additional fixes for ias and moat vpaid wrappers. (#34)
    * Additional fixes for ias and moat vpaid wrappers.
    
    * Linting

---
+++ @types/vpaid.d.ts
@@ -0,0 +1,20 @@
+interface VPAIDWrapper {
+    adParameters: string;
+    mediaFiles: VPAIDMediaFile[];
+}
+
+interface VPAIDMediaFile {
+    id: string;
+    delivery: 'progressive';
+    type: string;
+    bitrate: number;
+    width: number;
+    height: number;
+    scalable: boolean;
+    maintainAspectRatio: boolean;
+    uri: string;
+    apiFramework: string | null;
+    codec: string | null;
+    minBitrate?: number;
+    maxBitrate?: number;
+}
